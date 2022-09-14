a52f6506c28955d59634dc9c91f263f95e7d3896
iDoyoung <ido.young@icloud.com>
2022-09-14 14:35:03 +0900

    Add Collection View Cell

---
+++ DidTodayUIKit/Sources/Scenes/Main/DidCell.swift
@@ -0,0 +1,63 @@
+//
+//  DidCell.swift
+//  DidTodayUIKit
+//
+//  Created by Doyoung on 2022/09/13.
+//
+
+import UIKit
+
+class DidCell: UICollectionViewCell {
+    static let reuseIdentifier = "DidCellReuseIdentifier"
+    
+    let pieView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.font = .monospacedDigitSystemFont(ofSize: 20, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let contentLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    func configure() {
+        contentView.backgroundColor = .systemBackground
+        contentView.layer.cornerRadius = 20
+        pieView.frame = CGRect(origin: CGPoint(x: 0, y: 0),
+                               size: CGSize(width: contentView.frame.width / 1.8, height: contentView.frame.height / 1.8))
+        pieView.autoresizingMask = [.flexibleTopMargin,
+                                    .flexibleLeftMargin,
+                                    .flexibleWidth,
+                                    .flexibleHeight]
+        addSubview(pieView)
+        addSubview(timeLabel)
+        addSubview(contentLabel)
+        NSLayoutConstraint.activate([
+            timeLabel.topAnchor.constraint(equalTo: pieView.topAnchor),
+            timeLabel.bottomAnchor.constraint(equalTo: pieView.bottomAnchor),
+            timeLabel.leftAnchor.constraint(equalTo: pieView.rightAnchor, constant: 8),
+            timeLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -8),
+            contentLabel.topAnchor.constraint(equalTo: pieView.bottomAnchor),
+            contentLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            contentLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            contentLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8)
+        ])
+    }
+}
--- DidTodayUIKit/Sources/Scenes/Main/ViewController.swift
+++ DidTodayUIKit/Sources/Scenes/Main/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
+        configureDataSource()
+        applySnapShot()
     }
 }
 
@@ -25,20 +27,22 @@ extension ViewController: UICollectionViewDelegate {
     private func configureCollectionView() {
         didCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCollectionViewLayout())
         didCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        didCollectionView.backgroundColor = .brown
+        didCollectionView.backgroundColor = .secondarySystemBackground
         view.addSubview(didCollectionView)
     }
     private func createCollectionViewLayout() -> UICollectionViewCompositionalLayout {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                              heightDimension: .fractionalHeight(1.0))
+                                              heightDimension: .fractionalWidth(0.5))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                heightDimension: .fractionalHeight(1.0))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
-                                                       subitems: [item])
+                                                       subitem: item,
+                                                       count: 2)
+        group.interItemSpacing = .fixed(20)
+        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 12)
         let section = NSCollectionLayoutSection(group: group)
-        section.interGroupSpacing = 10
-        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 12, bottom: 10, trailing: 12)
         let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
     }
@@ -50,15 +54,23 @@ extension ViewController: UICollectionViewDelegate {
                                                                 item: itemIdentifier)
         }
     }
-    private func createCellRegistration() -> UICollectionView.CellRegistration<UICollectionViewCell, DidItem> {
-        return UICollectionView.CellRegistration<UICollectionViewCell, DidItem> { cell, indexPath, item in
-            
+    private func createCellRegistration() -> UICollectionView.CellRegistration<DidCell, DidItem> {
+        return UICollectionView.CellRegistration<DidCell, DidItem> { cell, indexPath, item in
+            cell.timeLabel.text = "00:00"
+            cell.contentLabel.text = "테스트 중"
         }
     }
     private func applySnapShot() {
         var snapShot = NSDiffableDataSourceSnapshot<Section, DidItem>()
         snapShot.appendSections([.main])
-        snapShot.appendItems([])
+        snapShot.appendItems([DidItem(id: UUID(),
+                                      started: Date(),
+                                      finished: Date(),
+                                      content: "",
+                                      red: 0,
+                                      green: 0,
+                                      blue: 0,
+                                      alpha: 0)])
         dataSource.apply(snapShot)
     }
     /// - Tag: Delegate

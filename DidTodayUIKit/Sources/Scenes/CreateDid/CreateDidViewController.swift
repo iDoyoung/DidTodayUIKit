2e2d2c616c257b6f20d9bc5c8e0eb9854937972f
iDoyoung <ido.young@icloud.com>
2022-09-19 16:48:37 +0900

    Add Text filed action with binding View model in Create Did View Controller

--- DidTodayUIKit/Sources/Scenes/CreateDid/CreateDidViewController.swift
+++ DidTodayUIKit/Sources/Scenes/CreateDid/CreateDidViewController.swift
@@ -6,8 +6,14 @@
 //
 
 import UIKit
+import Combine
 
 final class CreateDidViewController: UIViewController {
+    var viewModel: (CreateDidViewModelInput & CreateDidViewModelOutput)?
+    var cancellable: AnyCancellable?
+    
+    @IBOutlet weak var addButtonItem: UIBarButtonItem!
+    @IBOutlet weak var titleTextField: UITextField!
     
     @IBAction func showColorPicker(_ sender: UIButton) {
         let colorPickerViewController = UIColorPickerViewController()
@@ -23,8 +29,44 @@ final class CreateDidViewController: UIViewController {
     @IBAction func cancelAddDid(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
+    //MARK: - Life cycle
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        viewModel = CreateDidViewModel()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        viewModel = CreateDidViewModel()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTextFieldAction()
+        bindViewModel()
     }
-  
+    
+    //MARK: - Setup
+    private func setupTextFieldAction() {
+        titleTextField.addAction(textFieldAction(), for: .editingChanged)
+    }
+    private func textFieldAction() -> UIAction {
+        return UIAction { [weak self] _ in
+            guard let self = self else {
+                #if DEBUG
+                print("\(String(describing: self)) IS NIL")
+                #endif
+                return
+            }
+            self.viewModel?.title = self.titleTextField.text
+        }
+    }
+    private func bindViewModel() {
+        cancellable = viewModel?.titlePublisher
+            .sink { [weak self] title in
+            if title == nil || title == "" {
+                self?.addButtonItem.isEnabled = false
+            } else {
+                self?.addButtonItem.isEnabled = true
+            }
+        }
+    }
 }

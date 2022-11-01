80ba694843a10faf33bf35bcf677b6d86070c10b
iDoyoung <ido.young@icloud.com>
2022-11-01 20:08:47 +0900

    Add scroll to today

--- DidTodayUIKit/Sources/Scenes/Calendar/CalendarViewController.swift
+++ DidTodayUIKit/Sources/Scenes/Calendar/CalendarViewController.swift
@@ -30,18 +30,23 @@ class CalendarViewController: UIViewController {
         return viewController
     }
     
-    override func loadView() {
-        view = UIView()
-        view.backgroundColor = .systemBackground
-    }
+    //FIXME: - calendarView.scroll issue
+    ///loadView() 로 prent view 생성할 경우, viewDidLoad() 에서 calendarView.scroll 할 경우,
+    ///"Will attempt to recover by breaking constrain" Issue
+//    override func loadView() {
+//        view = UIView()
+//        view.backgroundColor = .systemBackground
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUIComponents()
+        calendarView.scroll(toDayContaining: Date(), scrollPosition: .centered, animated: false)
     }
     
     //MARK: - Configure
     private func configureUIComponents() {
+        view.backgroundColor = .systemBackground
         view.addSubview(calendarView)
         setupLayoutConstraints()
     }
@@ -51,16 +56,16 @@ class CalendarViewController: UIViewController {
         let calendar = Calendar.current
         //FIXME: - To date of first uploaded date
         let startDate = calendar.date(from: DateComponents(year: 2020, month: 01, day: 01))!
-        let endDate = Date().omittedTime()
+        let endDate = Date()
         
         return CalendarViewContent(
             calendar: calendar,
             visibleDateRange: startDate...endDate,
-            monthsLayout: .vertical(
+            monthsLayout: .vertical (
                 options: VerticalMonthsLayoutOptions(
-                    pinDaysOfWeekToTop: false,
-                    alwaysShowCompleteBoundaryMonths: false,
-                    scrollsToFirstMonthOnStatusBarTap: false)))
+                    pinDaysOfWeekToTop: true,
+                    alwaysShowCompleteBoundaryMonths: true,
+                    scrollsToFirstMonthOnStatusBarTap: true)))
         .monthHeaderItemProvider { month in
             CalendarItemModel<MonthLabel> (
                 invariantViewProperties: .init(

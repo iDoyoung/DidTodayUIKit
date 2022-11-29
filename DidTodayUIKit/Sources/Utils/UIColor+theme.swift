b46d92a0a2b0d19faba7ee38e03280e436f3ce65
iDoyoung <ido.young@icloud.com>
2022-11-29 12:08:45 +0900

    Add custom color background and green

--- DidTodayUIKit/Sources/Utils/UIColor+theme.swift
+++ DidTodayUIKit/Sources/Utils/UIColor+theme.swift
@@ -11,4 +11,12 @@ extension UIColor {
     class var themeGreen: UIColor {
         UIColor(red: 55/255, green: 162/255, blue: 112/255, alpha: 1)
     }
+    
+    class var customBackground: UIColor {
+        UIColor(named: "custom.background") ?? .systemBackground
+    }
+    
+    class var customGreen: UIColor {
+        UIColor(named: "custom.green") ?? .systemGreen
+    }
 }

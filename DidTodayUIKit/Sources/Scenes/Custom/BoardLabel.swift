f34e3f367a0824ee0c702e70db2ecca2f3842656
iDoyoung <ido.young@icloud.com>
2022-11-29 11:03:42 +0900

    Add animation and to set up UI

--- DidTodayUIKit/Sources/Scenes/Custom/BoardLabel.swift
+++ DidTodayUIKit/Sources/Scenes/Custom/BoardLabel.swift
@@ -9,23 +9,63 @@ import UIKit
 
 @IBDesignable final class BoardLabel: UIView {
     
-    @IBInspectable var text: String? {
-        didSet {
-            label.text = text
-        }
+    @IBInspectable var texts: [String]?
+   
+    @IBInspectable var font: UIFont {
+        get { label.font }
+        set { label.font = newValue }
+    }
+    
+    @IBInspectable var textColor: UIColor {
+        get { label.textColor }
+        set { label.textColor = newValue }
     }
     
     private let label: UILabel = {
         let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 2
         return label
     }()
     
+    private var isAnimated: Bool?
+    ///start animation
+    func startAnimation() {
+        isAnimated = true
+        guard let texts = texts, !texts.isEmpty else { return }
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            while self.isAnimated! {
+                self.texts?.forEach { text in
+                    #if DEBUG
+                    print("Changing Label Text..")
+                    #endif
+                    self.animatToLabelChange(to: text)
+                    sleep(4)
+                }
+            }
+        }
+    }
+   
+    func stopAnimation() {
+        isAnimated = false
+    }
+    
+    private func animatToLabelChange(to text: String) {
+        DispatchQueue.main.async { [weak self] in
+            self?.label.flipAnimation()
+            self?.label.text = text
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        configure()
     }
     
     private func configure() {
@@ -41,5 +81,16 @@ import UIKit
             label.bottomAnchor.constraint(equalTo: bottomAnchor),
             label.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
+    }
+}
+
+private extension UILabel {
+    func flipAnimation() {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        animation.type = .push
+        animation.subtype = .fromTop
+        animation.duration = 0.5
+        layer.add(animation, forKey: CATransitionType.push.rawValue)
     }
 }

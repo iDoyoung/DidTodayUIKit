fa674221ff0bba27c39af1851303a5cf144a9d5e
iDoyoung <ido.young@icloud.com>
2022-12-08 11:06:10 +0900

    Add UI to set Title and Color

--- DidTodayUIKit/Sources/Scenes/Doing/DoingViewController.swift
+++ DidTodayUIKit/Sources/Scenes/Doing/DoingViewController.swift
@@ -18,6 +18,8 @@ final class DoingViewController: UIViewController, StoryboardInstantiable {
     @IBOutlet weak var timerShadowEffectView: UIView!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var colorPickerButton: UIButton!
     
     @IBAction func done(_ sender: UIButton) {
         present(doneTimerAlert(), animated: true)
@@ -27,9 +29,17 @@ final class DoingViewController: UIViewController, StoryboardInstantiable {
         present(cancelTimerAlert(), animated: true)
     }
     
+    @IBAction func setPieColor(_ sender: UIButton) {
+        let colorPicker = UIColorPickerViewController()
+        colorPicker.supportsAlpha = false
+        colorPicker.delegate = self
+        present(colorPicker, animated: true)
+    }
+    
     //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        createDismissKeyboardTapGesuture()
         setupTimerView()
         bindViewModel()
     }
@@ -57,12 +67,39 @@ final class DoingViewController: UIViewController, StoryboardInstantiable {
     }
     
     private func bindViewModel() {
+        ///Bind With Timer Label
         viewModel?.timesOfTimer
             .receive(on: DispatchQueue.main)
-            .sink { [weak self] output in
-                self?.timerLabel.text = output
-            }
+            .sink { [weak self] output in self?.timerLabel.text = output }
             .store(in: &cancellableBag)
+        ///Bind With Done Button
+        viewModel?.doneIsEnabled
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] output in }
+            .store(in: &cancellableBag)
+        ///Bind Pie Color
+        viewModel?.colorOfPie
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] ouput in
+                guard let ouput = ouput else { return }
+                let color = UIColor.init(red: CGFloat(ouput.red),
+                                         green: CGFloat(ouput.green),
+                                         blue: CGFloat(ouput.blue),
+                                         alpha: CGFloat(ouput.alpha))
+                self?.colorPickerButton.tintColor = color
+                self?.timerLabel.textColor = color
+            })
+            .store(in: &cancellableBag)
+    }
+    
+    private func createDismissKeyboardTapGesuture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+ 
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 }
 
@@ -74,5 +111,25 @@ extension DoingViewController: TimerAlert {
     
     func doneTimer() {
         
+    }
+}
+
+extension DoingViewController: UIColorPickerViewControllerDelegate {
+    
+    func colorPickerViewController(_ viewController: UIColorPickerViewController, didSelect color: UIColor, continuously: Bool) {
+        guard let colorComponents = color.cgColor.components else { return }
+        viewModel?.setColorOfPie(red: Float(colorComponents[0]), green: Float(colorComponents[1]), blue: Float(colorComponents[2]))
+    }
+}
+
+extension DoingViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text , text != "" else { return }
+        viewModel?.setTitle(text)
     }
 }

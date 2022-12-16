47d40af609b5f6d5cbc8fb948ba258079d408b33
iDoyoung <ido.young@icloud.com>
2022-12-16 13:07:17 +0900

    Add Create Did Alert
    - Add Namespace of Create did view text

---
+++ DidTodayUIKit/Sources/Scenes/CreateDid/CreateDidAlert.swift
@@ -0,0 +1,42 @@
+//
+//  CreateDidAlert.swift
+//  DidTodayUIKit
+//
+//  Created by Doyoung on 2022/12/16.
+//
+
+import UIKit
+
+protocol CreateDidAlert {
+    func completeToCreateDid()
+    func discardToCreateDid()
+}
+
+extension CreateDidAlert {
+    
+    func completeToCreateDidAlert() -> UIAlertController {
+        let alert = UIAlertController(title: CreateDidViewText.completeToCreateAlertTitle,
+                                      message: CreateDidViewText.completeToCreateAlertMessage,
+                                      preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: CreateDidViewText.cancelActionTitle, style: .cancel)
+        let completeAction = UIAlertAction(title: CreateDidViewText.createActionTitle, style: .default) { _ in
+            completeToCreateDid()
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(completeAction)
+        return alert
+    }
+    
+    func discardToCreateDidAlert() -> UIAlertController {
+        let alert = UIAlertController(title: CreateDidViewText.discardToCreateDidAlertTitle,
+                                      message: CreateDidViewText.discardToCreateDidAlertMessage,
+                                      preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: CreateDidViewText.cancelActionTitle, style: .cancel)
+        let disCardAction = UIAlertAction(title: CreateDidViewText.discardActionTitle, style: .default) { _ in
+            discardToCreateDid()
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(disCardAction)
+        return alert
+    }
+}
---
+++ DidTodayUIKit/Sources/Utils/Namespaces/CreateDidViewText.swift
@@ -0,0 +1,22 @@
+//
+//  CreateDidViewText.swift
+//  DidTodayUIKit
+//
+//  Created by Doyoung on 2022/12/16.
+//
+
+import Foundation
+
+enum CreateDidViewText {
+    //MARK: - Alert
+    ///Alert View Controller
+    static let discardToCreateDidAlertTitle = "Discard your Creation?"
+    static let discardToCreateDidAlertMessage = ""
+    
+    static let completeToCreateAlertTitle = "Complete your Creation?"
+    static let completeToCreateAlertMessage = ""
+    ///Alert Action
+    static let cancelActionTitle = "Cancel"
+    static let discardActionTitle = "OK"
+    static let createActionTitle = "Create"
+}

46b57f404393256c6ad5f0578dee3ce88f632075
iDoyoung <ido.young@icloud.com>
2022-12-19 18:44:17 +0900

    Delete Text Field, Color Picker Button and Create Button With Relative Logic

--- DidTodayUIKit/Sources/Scenes/Doing/DoingViewController.swift
+++ DidTodayUIKit/Sources/Scenes/Doing/DoingViewController.swift
@@ -9,13 +9,7 @@ import UIKit
 import Combine
 
 final class DoingViewController: UIViewController, StoryboardInstantiable {
-    
-    enum EnableToComplete {
-        case titleIsEmpty
-        case lessThanOneMinutes
-        case enable
-    }
-    
+ 
     var viewModel: DoingViewModelProtocol?
     private var cancellableBag = Set<AnyCancellable>()
     
@@ -36,8 +30,6 @@ final class DoingViewController: UIViewController, StoryboardInstantiable {
     @IBOutlet weak var timerShadowEffectView: UIView!
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
-    @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var colorPickerButton: UIButton!
     @IBOutlet weak var informationBoardLabel: BoardLabel!
     @IBOutlet weak var startedTimeLabel: CircularLabel!
    
@@ -46,20 +38,12 @@ final class DoingViewController: UIViewController, StoryboardInstantiable {
     }
     
     @IBAction func done(_ sender: UIButton) {
-        switch checkEnableDoneButtonAction() {
-        case .enable:
-            present(doneTimerAlert(), animated: true)
-        case .titleIsEmpty:
-            occurFeedback()
-            titleTextField.animateToShake()
-        case .lessThanOneMinutes:
+        guard let viewModel = viewModel else { return }
+        if viewModel.isLessThanTime.value {
             occurFeedback()
             timerLabel.animateToShake()
-        case .none:
-            #if DEBUG
-            print("View Model Is Nil")
-            #endif
-            break
+        } else {
+            present(doneTimerAlert(), animated: true)
         }
     }
     
@@ -70,14 +54,6 @@ final class DoingViewController: UIViewController, StoryboardInstantiable {
     @IBAction func cancelDoing(_ sender: UIButton) {
         present(cancelTimerAlert(), animated: true)
     }
-    
-    @IBAction func setPieColor(_ sender: UIButton) {
-        let colorPicker = UIColorPickerViewController()
-        colorPicker.supportsAlpha = false
-        colorPicker.delegate = self
-        present(colorPicker, animated: true)
-    }
-    
     //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -135,49 +111,12 @@ final class DoingViewController: UIViewController, StoryboardInstantiable {
             .receive(on: DispatchQueue.main)
             .sink { [weak self] output in self?.timerLabel.text = output }
             .store(in: &cancellableBag)
-        ///Bind Pie Color
-        viewModel?.colorOfPie
-            .receive(on: DispatchQueue.main)
-            .sink(receiveValue: { [weak self] output in
-                self?.colorPickerButton.tintColor = output
-                self?.timerLabel.textColor = output
-            })
-            .store(in: &cancellableBag)
         ///Bind StartedTime
         viewModel?.startedTime
             .sink { [weak self] output in
                 self?.startedTimeLabel.text = output
             }
             .store(in: &cancellableBag)
-        ///Bind Result of Core Data
-        viewModel?.isSucceededCreated
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] result in
-                if result {
-                    self?.dismiss(animated: true)
-                }
-            }
-            .store(in: &cancellableBag)
-        viewModel?.error
-            .sink { error in
-                if error != nil {
-                    #if DEBUG
-                    print("Core Data Error: \(String(describing: error))")
-                    #endif
-                }
-            }
-            .store(in: &cancellableBag)
-    }
-    
-    private func checkEnableDoneButtonAction() -> EnableToComplete? {
-        guard let viewModel = viewModel else { return nil }
-        if viewModel.titleIsEmpty.value {
-            return .titleIsEmpty
-        } else if viewModel.isLessThanTime.value {
-            return .lessThanOneMinutes
-        } else {
-            return .enable
-        }
     }
     
     private func createDismissKeyboardTapGesture() {
@@ -198,29 +137,6 @@ extension DoingViewController: TimerAlert {
     }
     
     func doneTimer() {
-        viewModel?.createDid()
-    }
-}
-
-extension DoingViewController: UIColorPickerViewControllerDelegate {
-    
-    func colorPickerViewController(_ viewController: UIColorPickerViewController, didSelect color: UIColor, continuously: Bool) {
-        viewModel?.setColorOfPie(color)
-    }
-}
-
-extension DoingViewController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return false
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        guard let text = textField.text else { return }
-        if !text.trimmingCharacters(in: .whitespaces).isEmpty {
-            viewModel?.setTitle(text)
-        } else {
-            textField.text = ""
-        }
+        //TODO: Show Create Did
     }
 }

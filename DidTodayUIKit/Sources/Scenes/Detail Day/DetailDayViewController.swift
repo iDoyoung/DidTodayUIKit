96f8755b9058952bc199309d55ece4a731adfc8f
iDoyoung <ido.young@icloud.com>
2023-01-04 16:12:18 +0900

    Initial Detail Day View Controller

---
+++ DidTodayUIKit/Sources/Scenes/Detail Day/DetailDayViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailDayViewController.swift
+//  DidTodayUIKit
+//
+//  Created by Doyoung on 2023/01/04.
+//
+
+import UIKit
+
+final class DetailDayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+

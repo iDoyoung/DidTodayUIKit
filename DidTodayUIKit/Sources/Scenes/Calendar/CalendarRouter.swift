09c2dd230dea017fd5dec0bcdbcd1f504e079666
iDoyoung <ido.young@icloud.com>
2023-01-06 12:36:17 +0900

    Refactor To Fetch Dids From Core Data and Add Router That Set Calendar Flow

---
+++ DidTodayUIKit/Sources/Scenes/Calendar/CalendarRouter.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarRouter.swift
+//  DidTodayUIKit
+//
+//  Created by Doyoung on 2023/01/06.
+//
+
+import Foundation
+
+struct CalendarRouter {
+    let showDetailDay: (Did) -> Void
+}
--- DidTodayUIKit/Sources/Scenes/Calendar/ViewModel/CalendarViewModel.swift
+++ DidTodayUIKit/Sources/Scenes/Calendar/ViewModel/CalendarViewModel.swift
@@ -11,37 +11,50 @@ import Combine
 protocol CalendarViewModelProtocol: CalendarViewModelInput, CalendarViewModelOutput {   }
 
 protocol CalendarViewModelInput {
-    var dids: [Did] { get }
     var selectedDay: DateComponents? { get set }
 }
 
 protocol CalendarViewModelOutput {
+    var fetchedDids: CurrentValueSubject<[Did], Never> { get }
     var dateOfDids: CurrentValueSubject<[Date], Never> { get }
-    var didsOfDayItem: CurrentValueSubject<[DidsOfDayItemViewModel], Never> { get }
+    var itemsOfselectedDay: CurrentValueSubject<[DidsOfDayItemViewModel], Never> { get }
     var descriptionOfSelectedDay: CurrentValueSubject<String?, Never> { get }
     var startedDate: Date? { get }
 }
 
 final class CalendarViewModel: CalendarViewModelProtocol {
     
+    private var didCoreDataStorage: DidCoreDataStorable?
+    private var router: CalendarRouter?
     private var cancellableBag = Set<AnyCancellable>()
     
-    init(dids: [Did]) {
-        self.dids = dids
-        let dates = dids.map { $0.started.omittedTime() }
-        self.startedDate = dates.first ?? Date()
-        dateOfDids.send(dates)
+    init(didCoreDataStorage: DidCoreDataStorable, router: CalendarRouter) {
+        self.didCoreDataStorage = didCoreDataStorage
+        self.router = router
+        
+        fetchedDids
+            .sink { [weak self] dids in
+                let dates = dids.map { $0.started.omittedTime() }
+                self?.startedDate = dates.first ?? Date()
+                self?.dateOfDids.send(dates)
+            }
+            .store(in: &cancellableBag)
+        
         $selectedDay
             .sink { [weak self] day in
-                guard let day = day else { return }
-                let item = dids
-                    .filter { $0.started.omittedTime() == Calendar.current.date(from: day)?.omittedTime() }
-                    .map { DidsOfDayItemViewModel($0) }
-                self?.didsOfDayItem.send(item)
+                guard let theDay = day ,
+                      let theSelf = self else { return }
+                
+                let item = theSelf.fetchedDids.value
+                    .filter { $0.started.omittedTime() == Calendar.current.date(from: theDay)?.omittedTime() }
+                    .compactMap { DidsOfDayItemViewModel($0) }
+                theSelf.itemsOfselectedDay.send(item)
+                
+                /// - Tag: Setting Description Label
                 if item.isEmpty {
-                    self?.descriptionOfSelectedDay.send("Select Day")
+                    theSelf.descriptionOfSelectedDay.send("Select Day")
                 } else {
-                    self?.descriptionOfSelectedDay.send("Did \(item.count) Things")
+                    theSelf.descriptionOfSelectedDay.send("Did \(item.count) Things")
                 }
             }
             .store(in: &cancellableBag)
@@ -49,12 +62,26 @@ final class CalendarViewModel: CalendarViewModelProtocol {
     }
    
     //MARK: - Input
-    @Published var dids: [Did]
     @Published var selectedDay: DateComponents?
     
+    func fetchDids() {
+        didCoreDataStorage?.fetchDids { [weak self] dids, error in
+            guard let theSelf = self else { return }
+            if error == nil {
+                theSelf.fetchedDids.send(dids)
+            } else {
+                //TODO: Alert 사용해서 Core Data Fetch 실패를 알려야 하나
+                #if DEBUG
+                print("Error: \(String(describing: error))")
+                #endif
+            }
+        }
+    }
+    
     //MARK: - Output
+    var fetchedDids = CurrentValueSubject<[Did], Never>([])
     var dateOfDids = CurrentValueSubject<[Date], Never>([])
     var startedDate: Date?
     var descriptionOfSelectedDay = CurrentValueSubject<String?, Never>("Select Day")
-    var didsOfDayItem = CurrentValueSubject<[DidsOfDayItemViewModel], Never>([])
+    var itemsOfselectedDay = CurrentValueSubject<[DidsOfDayItemViewModel], Never>([])
 }

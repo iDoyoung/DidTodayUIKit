75cd3fba63c809e00cf54915df3c5c822dac834a
iDoyoung <ido.young@icloud.com>
2023-01-07 13:09:12 +0900

    Add Binding

--- DidTodayUIKit/Sources/Scenes/Detail Day/DetailDayViewController.swift
+++ DidTodayUIKit/Sources/Scenes/Detail Day/DetailDayViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Combine
 
 final class DetailDayViewController: DidListCollectionViewController {
 
     var viewModel: DetailDayViewModelProtocol?
+    private var cancellableBag = Set<AnyCancellable>()
+    
     //MARK: - Life Cycle
     static func create(with viewModel: DetailDayViewModelProtocol) -> DetailDayViewController {
         let viewController = DetailDayViewController()
@@ -23,14 +26,33 @@ final class DetailDayViewController: DidListCollectionViewController {
     }
     
     private func configure() {
-        setupView()
         configureCollectionView()
         configureDataSource()
+        bindViewModel()
     }
     
-    //MARK: - Setup
-    private func setupView() {
-        view.backgroundColor = .customBackground
+    override func configureCollectionView() {
+        super.configureCollectionView()
+        collectionView.backgroundColor = .customBackground
+    }
+    
+    //MARK: - Binding
+    private func bindViewModel() {
+        viewModel?.totalPieDids
+            .sink { [weak self] output in
+                self?.applyTotalDidSnapshot([output])
+            }
+            .store(in: &cancellableBag)
+        ///Bind Collection View with Did Item List
+        viewModel?.didItemsList
+            .sink { [weak self] output in
+                self?.applyDidListSnapshot(output)
+            }
+            .store(in: &cancellableBag)
+    }
+    
+    override func bindSortingSupplementaryWithViewModel(supplementary: SortingSupplementaryView) {
+        super.bindSortingSupplementaryWithViewModel(supplementary: supplementary)
     }
 }
 

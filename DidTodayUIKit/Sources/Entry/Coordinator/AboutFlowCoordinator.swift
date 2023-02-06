322e0ea8c9ec9f7754ac2059ccfb1f215973a0fc
iDoyoung <ido.young@icloud.com>
2023-02-06 12:27:43 +0900

    Fix URL of Open App Store To Write A Review

--- DidTodayUIKit/Sources/Entry/Coordinator/AboutFlowCoordinator.swift
+++ DidTodayUIKit/Sources/Entry/Coordinator/AboutFlowCoordinator.swift
@@ -39,7 +39,7 @@ final class AboutFlowCoordinator: Coordinator {
     }
     
     private func openAppStoreToWriteAReview() {
-        guard let url = URL(string: "https://apps.apple.com/app/id1625220721?action=write-review") else { return }
+        guard let url = URL(string: "https://apps.apple.com/app/id1549357218?action=write-review") else { return }
         if UIApplication.shared.canOpenURL(url) {
             UIApplication.shared.open(url)
         }

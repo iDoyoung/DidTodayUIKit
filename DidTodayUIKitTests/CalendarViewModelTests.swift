fecbf876cc6069d4cd595591f22e7b77b9e964ef
iDoyoung <ido.young@icloud.com>
2023-02-23 16:50:34 +0900

    Test Calendar View Model

--- DidTodayUIKitTests/CalendarViewModelTests.swift
+++ DidTodayUIKitTests/CalendarViewModelTests.swift
@@ -6,30 +6,105 @@
 //
 
 import XCTest
+import Combine
+@testable import DidTodayUIKit
 
 final class CalendarViewModelTests: XCTestCase {
 
+    var sut: CalendarViewModel!
+    var didsCoreDataStorageSpy: DidsCoreDataStorageSpy!
+    var coordinatorSpy: CoordinatorSpy!
+    var mockDids = [Seeds.Dids.christmasParty, Seeds.Dids.newYearParty, Seeds.Dids.todayDidMock2, Seeds.Dids.todayDidMock]
+    
+    private var cancellableBag = Set<AnyCancellable>()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        didsCoreDataStorageSpy = DidsCoreDataStorageSpy()
+        coordinatorSpy = CoordinatorSpy()
+        let router = CalendarRouter(showDetailDay: coordinatorSpy.showDetail(date:dids:))
+        sut = CalendarViewModel(didCoreDataStorage: didsCoreDataStorageSpy, router: router)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    //MARK: - Test Doubles
+    final class DidsCoreDataStorageSpy: DidCoreDataStorable {
+        
+        var dids: [Did]!
+        var createDidCalled = false
+        @Published var fetchDidsCalled = false
+        var updateDidCalled = false
+        var deleteDidCalled = false
+        
+        func create(_ did: Did, completion: @escaping (Did, CoreDataStoreError?) -> Void) {
+            createDidCalled = true
+        }
+        
+        func fetchDids() async throws -> [DidTodayUIKit.Did] {
+            fetchDidsCalled = true
+            dids = [Seeds.Dids.christmasParty, Seeds.Dids.newYearParty, Seeds.Dids.todayDidMock2, Seeds.Dids.todayDidMock]
+            return dids
+        }
+        
+        func update(_ did: Did, completion: @escaping (Did, CoreDataStoreError?) -> Void) {
+            updateDidCalled = true
+        }
+        
+        func delete(_ did: Did, completion: @escaping (Did, CoreDataStoreError?) -> Void) {
+            deleteDidCalled = true
         }
     }
-
+    
+    class CoordinatorSpy {
+        
+        var showDetailCalled = false
+        
+        func showDetail(date: Date, dids: [Did]) {
+            showDetailCalled = true
+        }
+    }
+    
+    //MARK: - Tests
+    func test_fetchDids_shouldCallStorage() {
+        let promise = expectation(description: "Storage Be Called")
+        didsCoreDataStorageSpy.$fetchDidsCalled
+            .sink { isCalled in
+                if isCalled {
+                    promise.fulfill()
+                }
+            }
+            .store(in: &cancellableBag)
+        
+        sut.fetchDids()
+        wait(for: [promise], timeout: 2)
+        
+        XCTAssertTrue(didsCoreDataStorageSpy.fetchDidsCalled)
+    }
+    
+    func test_fetchDids_shouldSendDateOfDidsWhenHaveDidsAndStartedDataHasValue() {
+        let promise = expectation(description: "Should send value")
+        sut.dateOfDids
+            .sink { dates in
+                if !dates.isEmpty {
+                    promise.fulfill()
+                }
+            }
+            .store(in: &cancellableBag)
+        sut.fetchDids()
+        wait(for: [promise], timeout: 2)
+        
+        let expectation = mockDids.map { $0.started.omittedTime() }
+        XCTAssertEqual(sut.dateOfDids.value, expectation)
+        XCTAssertNotNil(sut.startedDate)
+    }
+    
+    func test_showDetail_shouldCallCoordinator_whenSelectedDay() {
+        sut.selectedDay = DateComponents()
+        sut.showDetail()
+        XCTAssert(coordinatorSpy.showDetailCalled)
+    }
 }

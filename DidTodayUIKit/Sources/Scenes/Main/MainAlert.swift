326a091bbe85c24575c77f7eb37d362eeae2547d
iDoyoung <ido.young@icloud.com>
2023-02-27 15:50:29 +0900

    Add Main Alert

--- DidTodayUIKit/Sources/Scenes/Main/MainAlert.swift
+++ DidTodayUIKit/Sources/Scenes/Main/MainAlert.swift
@@ -5,4 +5,22 @@
 //  Created by Doyoung on 2023/02/27.
 //
 
-import Foundation
+import UIKit
+
+protocol MainAlert {
+    func okay()
+}
+
+extension MainAlert {
+    
+    func recordedBeforeAlert() -> UIAlertController {
+        let alert = UIAlertController(title: CustomText.recordedBeforeAlertTitle,
+                                      message: CustomText.recordedBeforeAlertMessage,
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: CustomText.okay, style: .default) {_ in
+            okay()
+        }
+        alert.addAction(action)
+        return alert
+    }
+}

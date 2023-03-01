7902c9fbf02c4fd2f58f2d6fe17e1152fe6f54cd
iDoyoung <ido.young@icloud.com>
2023-03-01 20:05:27 +0900

    Add Assert to debug

--- DidTodayUIKit/Sources/Entry/SceneDelegate.swift
+++ DidTodayUIKit/Sources/Entry/SceneDelegate.swift
@@ -14,6 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        assert(UserDefaults.standard.object(forKey: "Doing") == nil, "Do not have User Defaults data of key: \"Doing\"")
         window = UIWindow(windowScene: windowScene)
         let initialViewController = UINavigationController()
         window?.rootViewController = initialViewController

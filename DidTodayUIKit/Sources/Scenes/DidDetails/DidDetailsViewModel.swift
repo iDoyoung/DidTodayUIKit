641c42263b1a5f8081ecd4ac1a18c7645ae47c53
iDoyoung <ido.young@icloud.com>
2023-03-11 19:47:28 +0900

    Add Did Details View Model

---
+++ DidTodayUIKit/Sources/Scenes/DidDetails/DidDetailsViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  DidDetailsViewModel.swift
+//  DidTodayUIKit
+//
+//  Created by Doyoung on 2023/03/11.
+//
+
+import Foundation
+
+protocol DidDetailsViewModelProtocol: DidDetailsViewModelInput, DidDetailsViewModelOutput { }
+
+protocol DidDetailsViewModelInput {
+    
+}
+
+protocol DidDetailsViewModelOutput {
+    
+}
+
+final class DidDetailsViewModel: DidDetailsViewModelProtocol {
+    
+}

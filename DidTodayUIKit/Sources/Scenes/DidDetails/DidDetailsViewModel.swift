b57ffc94ec609604646d0b1148811f0156422e64
iDoyoung <ido.young@icloud.com>
2023-03-12 11:53:06 +0900

    Implement Output of Did Details View Model

--- DidTodayUIKit/Sources/Scenes/DidDetails/DidDetailsViewModel.swift
+++ DidTodayUIKit/Sources/Scenes/DidDetails/DidDetailsViewModel.swift
@@ -6,17 +6,37 @@
 //
 
 import Foundation
+import Combine
 
 protocol DidDetailsViewModelProtocol: DidDetailsViewModelInput, DidDetailsViewModelOutput { }
 
-protocol DidDetailsViewModelInput {
-    
-}
+protocol DidDetailsViewModelInput { }
 
 protocol DidDetailsViewModelOutput {
-    
+    var date: Just<String> { get }
+    var title: Just<String> { get }
+    var didTime: Just<String> { get }
+    var timeRange: Just<String> { get }
 }
 
 final class DidDetailsViewModel: DidDetailsViewModelProtocol {
+
+    //MARK: Output
+    var date: Just<String>
+    var title: Just<String>
+    var didTime: Just<String>
+    var timeRange: Just<String>
     
+    init(_ did: Did) {
+        let startDate = did.started.toString()
+        let startedTime = did.started.currentTimeToString()
+        let finishedTime = did.finished.currentTimeToString()
+        ///date components of  difference between started and finished.
+        let components = Calendar.current.dateComponents([.hour, .minute], from: did.finished, to: did.started)
+
+        date = Just(startDate)
+        title = Just(did.content)
+        didTime = Just("Did \(components.hour ?? 0): \(components.minute ?? 0)")
+        timeRange = Just("\(startedTime) - \(finishedTime)")
+    }
 }
---
+++ DidTodayUIKitTests/Scenes/DidDetails/DidDetailsViewModelTests.swift
@@ -0,0 +1,80 @@
+//
+//  DidDetailsViewModelTests.swift
+//  DidTodayUIKitTests
+//
+//  Created by Doyoung on 2023/03/11.
+//
+
+import XCTest
+@testable import DidTodayUIKit
+
+final class DidDetailsViewModelTests: XCTestCase {
+    
+    //MARK: - System Under Test
+    var sut: DidDetailsViewModel!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    func test_initailize_shouldBeGetOutputOfDate() {
+        let promise = expectation(description: "Get output of Date")
+        ///given
+        let mockDid = Seeds.Dids.christmasParty
+        ///when
+        sut = DidDetailsViewModel(mockDid)
+        ///then
+        let _ = sut.date.sink { result in
+            XCTAssertEqual(result, "Dec 25, 2021")
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 1)
+    }
+    
+    func test_initailize_shouldBeGetOutputOfTitle() {
+        let promise = expectation(description: "Get output of Title")
+        ///given
+        let mockDid = Seeds.Dids.christmasParty
+        ///when
+        sut = DidDetailsViewModel(mockDid)
+        ///then
+        let _ = sut.title.sink { result in
+            XCTAssertEqual(result, "Christmas Party")
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 1)
+    }
+    
+    func test_initailize_shouldBeGetOutputOfDidTime() {
+        let promise = expectation(description: "Get output of Did Time")
+        ///given
+        let mockDid = Seeds.Dids.christmasParty
+        ///when
+        sut = DidDetailsViewModel(mockDid)
+        ///then
+        let _ = sut.didTime.sink { result in
+            XCTAssertEqual(result, "Did 0: 0")
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 1)
+    }
+    
+    func test_initailize_shouldBeGetOutputOfTimeRange() {
+        let promise = expectation(description: "Get output of Time Range")
+        ///given
+        let mockDid = Seeds.Dids.christmasParty
+        ///when
+        sut = DidDetailsViewModel(mockDid)
+        ///then
+        let _ = sut.timeRange.sink { result in
+            XCTAssertEqual(result, "12:00 AM - 12:00 AM")
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 1)
+    }
+}

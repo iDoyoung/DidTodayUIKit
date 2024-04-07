4c4615250fdc1849caebe41c44476d847828104a
iDoyoung <ido.young@icloud.com>
2024-04-07 12:26:05 +0900

    Add Color Property in Did Model

--- DidTodayUIKit/Sources/Models/Did.swift
+++ DidTodayUIKit/Sources/Models/Did.swift
@@ -6,14 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct Did: Equatable, Hashable {
+struct Did: Identifiable, Equatable, Hashable {
+    
     let id: UUID
     let withTimer: Bool
     let started: Date
     let finished: Date
     var content: String
     var pieColor: PieColor
+    
+    //FIXME: - UI 요소를 모델 레이어에서 설정하는게 맞을까?
+    var uiColor: UIColor {
+        UIColor(
+            red: CGFloat(pieColor.red),
+            green: CGFloat(pieColor.green),
+            blue: CGFloat(pieColor.blue),
+            alpha: CGFloat(pieColor.alpha)
+        )
+    }
+    
+    var color: Color {
+        Color(uiColor: uiColor)
+    }
     
     static func == (lhs: Did, rhs: Did) -> Bool {
         lhs.id == rhs.id
@@ -27,6 +43,7 @@ struct Did: Equatable, Hashable {
         self.content = content
         self.pieColor = color
     }
+    
     init(id: UUID, withTimer: Bool = false, started: Date, finished: Date, content: String, red: Float, green: Float, blue: Float, alpha: Float) {
         self.id = id
         self.withTimer = withTimer

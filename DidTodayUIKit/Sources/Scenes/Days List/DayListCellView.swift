f6a4a8a39fcd532b03821160d6bfbd9877c2478e
iDoyoung <ido.young@icloud.com>
2024-05-13 14:57:03 +0900

    Implement Day List Cell View

---
+++ DidTodayUIKit/Sources/Scenes/Days List/DayListCellView.swift
@@ -0,0 +1,83 @@
+import SwiftUI
+
+struct DayListCellView: View {
+    
+    @Binding var color: Color
+    @Binding var dids: [Did]
+    
+    @State var month: String
+    @State var day: String
+    @State var year: String
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Text(year)
+                    .frame(
+                        maxWidth: .infinity,
+                        alignment: .leading
+                    )
+                    .padding(.vertical, 1)
+                
+                Text(month)
+                    .font(
+                        .system(
+                            size: 30,
+                            weight: .medium,
+                            design: .rounded
+                        )
+                    )
+                    .frame(
+                        maxWidth: .infinity,
+                        alignment: .leading
+                    )
+                    .padding(.bottom, -30)
+                
+                Text(day)
+                    .font(
+                        .system(
+                            size: 60,
+                            weight: .medium,
+                            design: .rounded
+                        )
+                    )
+                    .frame(
+                        maxWidth: .infinity,
+                        alignment: .leading
+                    )
+                    .padding(.bottom)
+            }
+            .padding(.leading)
+            
+            RoundedRectangle(cornerRadius: 1)
+                .frame(width: 2, height: 100)
+                .padding()
+            
+            DidsSimpleChartView(dids: $dids)
+                .padding(.top)
+        }
+        .padding()
+        .background(color)
+        .clipShape(RoundedRectangle(cornerRadius: 25))
+    }
+}
+
+#Preview {
+    
+    @State var date = Date().toString()
+    @State var count = 5
+    @State var color = Color.blue
+    @State var dids = [
+        Did(started: Calendar.current.startOfDay(for: Date()), finished: .init(), content: "Did 1", color: .systemRed),
+        Did(started: Calendar.current.startOfDay(for: Date()), finished: .init(), content: "Did 2", color: .systemBlue),
+        Did(started: Calendar.current.startOfDay(for: Date()), finished: .init(), content: "Did 3", color: .systemGray)
+    ]
+    
+    return DayListCellView(
+        color: $color,
+        dids: $dids,
+        month: "May",
+        day: "5",
+        year: "2024"
+    )
+}
